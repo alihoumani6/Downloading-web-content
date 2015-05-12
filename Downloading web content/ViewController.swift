c0c706649d4b928c333033e162c23e3a862067d3
Ali Houmani <alihoumani6@gmail.com>
2015-05-12 18:25:51 -0400

    Uploaded May 12

--- Downloading web content/ViewController.swift
+++ Downloading web content/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //define the url
+        let url = NSURL(string: "http://www.reddit.com/r/all.compact")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!){
+            (data, response, error) in
+            
+            if (error == nil)
+            {
+                //converts to readable format
+                var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+                
+                println(urlContent)
+                
+                dispatch_async(dispatch_get_main_queue()){
+                
+                self.webView.loadHTMLString(urlContent! as String, baseURL: nil)
+            }
+            }
+            
+            
+            
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
